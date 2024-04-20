6ed10f69a6d5ab96d66dd549a87880a903689069
Ksenia Ponomareva <ponomarevaks@gmail.com>
2024-04-20 18:19:32 +0200

    test

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Link, Outlet } from 'react-router-dom';
+
+
+const App = () => (
+  <div>
+    <h1>Super Store</h1>
+    <nav>
+      <Link to="/about">About</Link> |{' '}
+      <Link to="/contact">Contact</Link>
+    </nav>
+    <main>
+      <Outlet />
+    </main>
+  </div>
+);
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+
+
+function About() {
+  return (
+    <>
+      <h2>About</h2>
+      <p>Lorem ipsum...</p>
+    </>
+  );
+}
+
+function Contact() {
+  return (
+    <>
+      <h2>Contact</h2>
+      <p>Lorem ipsum...</p>
+    </>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'Contact',
+        element: <Contact />,
+      },
+    ]
+  },
+  {
+    basename: "/my-app"
+  }
+]);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
